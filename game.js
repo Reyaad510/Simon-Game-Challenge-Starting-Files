70d8da93ccfdc668c5a9683075ab3d7a5526f5c7
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-11-30 23:12:13 -0500

    Created rng of random color to the game pattern

--- game.js
+++ game.js
@@ -2,3 +2,18 @@
 // $("h1").text("hi");
 
 
+const buttonColors = ["red", "blue", "green", "yellow"];
+const gamePattern = [];
+
+
+const nextSequence = () => {
+
+    let randomNumber = Math.floor(Math.random() * 4);
+
+    gamePattern.push(buttonColors[randomNumber]);
+    console.log(gamePattern);
+
+
+}
+
+nextSequence();
