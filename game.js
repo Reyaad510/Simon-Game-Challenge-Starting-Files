9e093fd7fc041bb32a7a846974f867112ddb67f2
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-11-30 23:39:18 -0500

    Added animation and sound for color

--- game.js
+++ game.js
@@ -8,12 +8,42 @@ const gamePattern = [];
 
 const nextSequence = () => {
 
+    // Random number between 0 and 3
     let randomNumber = Math.floor(Math.random() * 4);
 
-    gamePattern.push(buttonColors[randomNumber]);
-    console.log(gamePattern);
+    // Random color chosen from the buttonColors array 
+    let randomChosenColor = buttonColors[randomNumber]
 
+
+    animateColor(randomChosenColor);
+    playSound(randomChosenColor);
+
+
+    // Pushing random color to end of gamePattern array
+    gamePattern.push(randomChosenColor);
+    console.log(gamePattern);
 
 }
 
+
+
+const playSound = (sound) => {
+
+    // Play correct sound for specific colored button
+    let audio = new Audio(`sounds/${sound}.mp3`);
+    audio.play();
+
+}
+
+
+const animateColor = (color) => {
+
+    // animate chosen color to blink
+    $(`#${color}`).fadeOut(0.001).fadeIn();
+
+}
+
+
 nextSequence();
+
+
